c9fa1b0f73b2711dc60825779e28a7350676bce1
lwod <lwodcurrentmail@gmail.com>
2021-04-08 19:39:23 +0300

    test commit with server and client (build) parts

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+const http = require('http')
+const server = http.createServer(app)
+
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'client','build')))
+app.get('/', async (req,res)=>{
+	res.sendFile(`${__dirname}/client/build/index.html`)
+})
+
+
+const io = require('socket.io')(server)
+io.on('connection', socket => {
+	
+	socket.on('message', ({name, message})=>{
+		io.emit('message', {name, message})
+	})
+	
+})
+
+server.listen(port, ()=>{
+	console.log('server started')
+})
